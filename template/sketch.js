9c698c0c3a58a25a0e017ccaca61d4bdab483a0b
spngbob <54913045+spngbob@users.noreply.github.com>
2019-11-14 12:12:18 -0600

    added template

---
+++ template/sketch.js
@@ -0,0 +1,14 @@
+// JUST A TEMPLATE TO COPY AND PASTE
+// Jenna Doucette
+// Date
+
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+function setup() {  
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
