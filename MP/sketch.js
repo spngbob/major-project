d015292ccaf92583cc99c727b7f773d783935cc9
spngbob <54913045+spngbob@users.noreply.github.com>
2019-11-19 14:11:35 -0600

    made "sprite", made it change balance when hovering over it
    also laid down some functions, and made some variables that will help me later.

--- MP/sketch.js
+++ MP/sketch.js
@@ -1,5 +1,17 @@
 // major project
-// started november 14th?
+// started november 14th
+
+
+let grid = [];
+let level; 
+let levelOne;
+let balance = 0; // money balance
+let income = 3; // how much your income is, going to be changed. 
+let incomeSpeed = 2; // how often you get your "paycheck" 
+let bonus = 1;
+let beans = 0;
+let costIncrease = 1.1;
+
 
 
 function setup() {  
@@ -8,4 +20,25 @@ function setup() {
 
 function draw() {
   background(220);
+  createSprite(levelOne); // shows image/sprite to be clicked on 
+}
+
+
+function createSprite(level){
+  noStroke();
+  rect(width/2-50, height/2-50, 100, 100);
+  fill(100);
+  mouseClicked();
+}
+
+
+
+function mouseClicked(){
+  if (mouseX > width/2 - 50 && mouseX < width/2 + 50 && mouseY > height/2 - 50 && mouseY < height/2 + 50) {
+    balance = balance + bonus;
+    console.log(balance);
+    fill(50);
+  
+  }
+
 }
