3d0da034d286de58ee273fbae7e0c115f9ac2743
spngbob <54913045+spngbob@users.noreply.github.com>
2019-11-20 13:59:17 -0600

    figured out my balance and clicking

--- MP/sketch.js
+++ MP/sketch.js
@@ -1,44 +1,50 @@
 // major project
+// jenna doucette
 // started november 14th
 
-
-let grid = [];
-let level; 
-let levelOne;
+let level = 5;
+let levels = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+let levelOne; // level 
 let balance = 0; // money balance
 let income = 3; // how much your income is, going to be changed. 
 let incomeSpeed = 2; // how often you get your "paycheck" 
 let bonus = 1;
-let beans = 0;
 let costIncrease = 1.1;
-
-
 
 function setup() {  
   createCanvas(windowWidth, windowHeight);
 }
 
+function windowResized(){
+  resizeCanvas(windowWidth, windowHeight);
+}
+
 function draw() {
   background(220);
-  createSprite(levelOne); // shows image/sprite to be clicked on 
+  createSprite(); // shows image/sprite to be clicked on 
 }
 
-
-function createSprite(level){
-  noStroke();
+function createSprite(){
+  // creates the current square-esque sprite
   rect(width/2-50, height/2-50, 100, 100);
-  fill(100);
-  mouseClicked();
+  fill(levels[level]);
 }
 
-
-
-function mouseClicked(){
+function mouseClicked(){ // changes balance rigght now based on how many times you clicked. 
   if (mouseX > width/2 - 50 && mouseX < width/2 + 50 && mouseY > height/2 - 50 && mouseY < height/2 + 50) {
     balance = balance + bonus;
     console.log(balance);
-    fill(50);
-  
+    fill(0, 0, 0, 150);
   }
+}
 
+class sellButtons{
+  constructor(x, y, w, h, colour, text){
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.colour = colour;
+    // check if clicked 
+    // colour change
+  }
 }
