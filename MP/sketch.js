5ccff7f043b891e055ad354a32e6389865b6be86
spngbob <54913045+spngbob@users.noreply.github.com>
2019-11-21 14:16:03 -0600

    added array to try and make upgrades, isnt working yet :(

--- MP/sketch.js
+++ MP/sketch.js
@@ -2,7 +2,7 @@
 // jenna doucette
 // started november 14th
 
-let level = 5;
+let level = 0;
 let levels = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 let levelOne; // level 
 let balance = 0; // money balance
@@ -10,6 +10,9 @@ let income = 3; // how much your income is, going to be changed.
 let incomeSpeed = 2; // how often you get your "paycheck" 
 let bonus = 1;
 let costIncrease = 1.1;
+let baseCost = 50;
+
+let sales = [];
 
 function setup() {  
   createCanvas(windowWidth, windowHeight);
@@ -22,10 +25,25 @@ function windowResized(){
 function draw() {
   background(220);
   createSprite(); // shows image/sprite to be clicked on 
+  console.log(balance);
+  textSize(((height+width)/2)/50);
+  text("$" + balance, windowWidth/2-25, windowHeight/2-windowHeight/6);
+  
 }
+
+function createSale(){
+  for (let i = 0; i < 100; i ++){
+    let salebutton = new upgrade(width/2-50, height/2-50, 200, 200, 255, 0, 0, baseCost)
+    baseCost = baseCost * costIncrease;
+    sales.push(saleshutton);
+    salebutton.display();
+  }
+}
+
 
 function createSprite(){
   // creates the current square-esque sprite
+  noStroke();
   rect(width/2-50, height/2-50, 100, 100);
   fill(levels[level]);
 }
@@ -33,18 +51,37 @@ function createSprite(){
 function mouseClicked(){ // changes balance rigght now based on how many times you clicked. 
   if (mouseX > width/2 - 50 && mouseX < width/2 + 50 && mouseY > height/2 - 50 && mouseY < height/2 + 50) {
     balance = balance + bonus;
-    console.log(balance);
     fill(0, 0, 0, 150);
+  }
+  if (balance > 50){
+    createSale();
   }
 }
 
-class sellButtons{
-  constructor(x, y, w, h, colour, text){
+class upgrade{
+  constructor(x, y, xl, yl, r, g, b, cost){
     this.x = x;
     this.y = y;
-    this.w = w;
-    this.colour = colour;
-    // check if clicked 
-    // colour change
+    this.xl = xl;
+    this.yl = yl; 
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.cost = cost;
+  }
+  
+  display(){
+    noStroke();
+    fill(this.r, this.g, this.b, 100);
+    rect(this.x, this.y, this.xl, this.yl)
+  }
+}
+
+function createSale(){
+  for (let i = 0; i < 1; i ++){
+    let salebutton = new upgrade(width/2-50, height/2-50, 200, 200, 255, 0, 0, baseCost)
+    baseCost = baseCost * costIncrease;
+    sales.push(salebutton);
+    salebutton.display();
   }
 }
