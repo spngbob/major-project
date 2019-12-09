82c657a3705085863776ecc17c2fd896923850fe
Doucette2502259 <2502259@sps-student.sk.ca>
2019-12-09 14:12:28 -0600

    broke it, think i made the upgrade system better but its not working yet

--- MP/sketch.js
+++ MP/sketch.js
@@ -2,13 +2,13 @@
 // jenna doucette
 // started november 14th 
 
-let level = 6;
-let levelscol = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 let balance = 0; // money balance
 let income = 3; // how much your income is, going to be changed. 
 let bonus = 1;
-let costIncrease = 1.1;
-let baseCost = 50;
+let cost = 50;
+let costIncrease = 100;
+
+
 
 let sales = [];
 
@@ -16,9 +16,11 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
+
 function windowResized(){
   resizeCanvas(windowWidth, windowHeight);
 }
+
 
 function draw() {
   background(220);
@@ -29,11 +31,11 @@ function draw() {
   
 }
 
+
 function createSale(){
   for (let i = 0; i < 100; i ++){
-    let salebutton = new upgrade(width/2-50, height/2-50, 200, 200, 255, 0, 0, baseCost)
-    baseCost = baseCost * costIncrease;
-    sales.push(saleshutton);
+    let salebutton = new upgrade(width/2-50, height/2-50, 200, 200, 255, 0, 0, cost)
+    sales.push(salebutton);
     salebutton.display();
   }
 }
@@ -43,19 +45,22 @@ function createSprite(){
   // creates the current square-esque sprite
   noStroke();
   rect(width/2-50, height/2-50, 100, 100);
-  fill(level[levelscol]);
+  fill("red");
 }
+
 
 function mouseClicked(){ // changes balance rigght now based on how many times you clicked. 
   if (mouseX > width/2 - 50 && mouseX < width/2 + 50 && mouseY > height/2 - 50 && mouseY < height/2 + 50) {
-    balance = balance + bonus;
+    balance = balance + income;
     fill(0, 0, 0, 150);
-  }
-
-  if (balance === baseCost ){
-    let baseCost = costIncrease * baseCost;
-    createSale();
-    console.log("yeehaw");
+    checkBalance();
   }
 }
 
+
+function checkBalance(){
+  if (balance === cost){
+    let cost = costIncrease + cost;
+    createSale();
+  }
+}
