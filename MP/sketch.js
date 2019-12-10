b2ac264522770f46ae72cd2eee10c10aba90e0f1
Doucette2502259 <2502259@sps-student.sk.ca>
2019-12-10 14:12:24 -0600

    added picture to background, broke counter though

--- MP/sketch.js
+++ MP/sketch.js
@@ -2,6 +2,17 @@
 // jenna doucette
 // started november 14th 
 
+
+
+// income rules;
+
+// income level one = 1 
+// income level two = 2
+// income level three = 4
+// income level four = 8
+// income level five = 16 and etc etc etc !!!!!!!
+
+
 let balance = 0; // money balance
 let income = 3; // how much your income is, going to be changed. 
 let bonus = 1;
@@ -12,8 +23,13 @@ let costIncrease = 100;
 
 let sales = [];
 
+function preload() {
+  img = loadImage('assets/background.jfif');
+}
+
 function setup() {  
   createCanvas(windowWidth, windowHeight);
+  image(img, 100, 100);
 }
 
 
@@ -23,7 +39,6 @@ function windowResized(){
 
 
 function draw() {
-  background(220);
   createSprite(); // shows image/sprite to be clicked on 
   console.log(balance);
   textSize(((height+width)/2)/50);
@@ -33,12 +48,10 @@ function draw() {
 
 
 function createSale(){
-  for (let i = 0; i < 100; i ++){
-    let salebutton = new upgrade(width/2-50, height/2-50, 200, 200, 255, 0, 0, cost)
-    sales.push(salebutton);
-    salebutton.display();
+  let salebutton = new upgrade(cost, "red");
+  sales.push(salebutton);
+  salebutton.display();
   }
-}
 
 
 function createSprite(){
@@ -64,3 +77,19 @@ function checkBalance(){
     createSale();
   }
 }
+
+
+
+class upgrade {
+  constructor(money, fillColor) {
+    this.fillColor = color(0);
+    this.money = cost; 
+    this.x = width/2;
+    this.y = height/2;
+  }
+
+  makeRect() {
+    rect(this.x - 20, this.y - 20, this.x + 20, this.y + 20);
+    fill(this.fillColor)
+  }
+  }
