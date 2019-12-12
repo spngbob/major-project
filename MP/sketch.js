a9814eae661f3eb9f121dda73af5db7cbfc7e87a
Doucette2502259 <2502259@sps-student.sk.ca>
2019-12-12 12:24:05 -0600

    got rid of class, worked on shop upgrades, aint working yet :frowning:
    IT NOW POPS UP KIND OF INCORREXTLY BUT ITS THERE

--- MP/sketch.js
+++ MP/sketch.js
@@ -3,21 +3,13 @@
 // started november 14th 
 
 
-
-// income rules;
-
-// income level one = 1 
-// income level two = 2
-// income level three = 4
-// income level four = 8
-// income level five = 16 and etc etc etc !!!!!!!
-
-
 let money = 0; // money balance
-let income = 3; // how much your income is, going to be changed. 
+let income = 15; // how much your income is, going to be changed. 
 let bonus = 1;
 let cost = 50;
-let costIncrease = 100;
+let costIncrease = 70;
+let x = windowWidth;
+let y = windowHeight;
 
 
 
@@ -32,7 +24,8 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   imageMode(CENTER);
   image(img, windowWidth/2, windowHeight/2, windowWidth, windowHeight);
-  image(sprite, windowWidth/2, windowHeight/2, 100, 100 );
+  imageMode((CENTER))
+  image(sprite, windowWidth/2, windowHeight/2, 100, 100);
   fill(255);
 }
 
@@ -45,51 +38,22 @@ function windowResized(){
 function draw() {
   console.log(money);
   textSize(((height+width)/2)/30);
-  text("$" + money, windowWidth/2-25, windowHeight/2-windowHeight/6);
-  
-}
-
-
-function createSale(){
-  let salebutton = new upgrade(cost, "red");
-  sales.push(salebutton);
-  salebutton.display();
-  }
+  text("$" + money, windowWidth/2-50, windowHeight/2-windowHeight/6);
+  if (money >= cost){
+    cost = costIncrease + cost;
+    rect(windowWidth/4, windowHeight/3+windowHeight/3, 2*windowWidth/4, 2*windowHeight/2);
+    console.log("WOWWWWWWWWWWWWWWWWWWWWWWWWWWW");
+  }}
 
 
 
 
 
-function mouseClicked(){ // changes balance rigght now based on how many times you clicked. 
+
+function mousePressed(){ // changes balance rigght now based on how many times you clicked. 
   if (mouseX > width/2 - 50 && mouseX < width/2 + 50 && mouseY > height/2 - 50 && mouseY < height/2 + 50) {
     money = money + income;
-    checkBalance();
     image(sprite, windowWidth/2, windowHeight/2, 140, 140 );
     setup();
-
   }
 }
-
-
-function checkBalance(){
-  if (money === cost){
-    let cost = costIncrease + cost;
-    createSale();
-  }
-}
-
-
-
-class upgrade {
-  constructor(money, fillColor) {
-    this.fillColor = color(0);
-    this.money = cost; 
-    this.x = width/2;
-    this.y = height/2;
-  }
-
-  makeRect() {
-    rect(this.x - 20, this.y - 20, this.x + 20, this.y + 20);
-    fill(this.fillColor)
-  }
-  }
