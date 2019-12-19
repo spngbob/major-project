f364bac4e599224a22aa2fb5997f52f0a48b64c3
Doucette2502259 <2502259@sps-student.sk.ca>
2019-12-19 14:16:00 -0600

    ADDED NEW UPGRADES AND NEW SPRITES

--- MP/sketch.js
+++ MP/sketch.js
@@ -12,8 +12,25 @@ let costIncrease = 5 * bonus;
 let sales = [];
 
 function preload() {
-  img = loadImage('assets/background.jfif');
-  sprite = loadImage('assets/actualcookie.png');
+  sprite = loadImage('assets/sprite.png'); //main cookie to click on
+  background = loadImage('assets/kitchen.png');
+  b1 = loadImage('assets/baker_1.png');   // all the bakers are different upgrades
+  b2 = loadImage('assets/baker_2.png');
+  b3 = loadImage('assets/baker_3.png');
+  b4 = loadImage('assets/baker_4.png');
+  b5 = loadImage('assets/baker_5.png');
+  b6 = loadImage('assets/baker_6.png');
+  b7 = loadImage('assets/baker_7.png');
+  b8 = loadImage('assets/baker_8.png');
+  bowl = loadImage('assets/bowl.png');
+  cookie = loadImage('assets/cookie.png');
+  cup = loadImage('assets/cup.png');
+  
+
+
+
+
+
   let x = windowWidth;
   let y = windowHeight;
 }
@@ -21,11 +38,10 @@ function preload() {
 
 function setup() {  
   createCanvas(windowWidth, windowHeight);
-  imageMode(CENTER);
-  image(img, windowWidth/2, windowHeight/2, windowWidth, windowHeight);
   imageMode((CENTER))
+  image(background, windowWidth/2, windowHeight/2 , windowWidth, windowHeight)
   image(sprite, windowWidth/2, windowHeight/2, 100, 100);
-  fill(255);
+  fill("black");
 }
 
 
@@ -39,19 +55,51 @@ function draw() {
   console.log(money);
   textSize(((height+width)/2)/30);
   text("$" + money, windowWidth/2-50, windowHeight/2-windowHeight/6);
-  
+  text("$ " + cost + " = next upgrade", 300, 100);
 }
 
 
 function mousePressed(){ // changes balance rigght now based on how many times you clicked. 
-  if (mouseX > width/2 - 50 && mouseX < width/2 + 50 && mouseY > height/2 - 50 && mouseY < height/2 + 50) {
+  if (mouseX > width/2 - 50 && mouseX < width/2 + 50 && mouseY > height/2 - 50 && mouseY < height/2 + 50 || keyIsPressed === true) {
     money = money + income;
     image(sprite, windowWidth/2, windowHeight/2, 140, 140 );
     setup();
   }
   if (money >= cost){
     noStroke();
-    rect(100, 100, 100, 100);
+    if (cost < 1000){
+      image(b1, 150, 150, 100, 100)
+
+    }
+
+    else if (cost < 2500){
+      image(b2, 150, 150, 100, 100)
+  
+    }
+    else if (cost < 7500){
+      image(b3, 150, 150, 100, 100)
+
+    }
+    else if (cost < 10000){
+      image(b4, 150, 150, 100, 100)
+
+    }
+    else if (cost < 100000){
+      image(b5, 150, 150, 100, 100)
+
+    }
+    else if (cost < 2000000){
+      image(b6, 150, 150, 100, 100)
+
+    }
+    else if (cost < 3000000){
+      image(b7, 150, 150, 100, 100)
+
+    }
+    else{
+      image(b8, 150, 150, 100, 100)
+
+    }
     if ((mouseX > 100 && mouseX < 200 && mouseY > 100&& mouseY < 200) &&  mouseIsPressed === true){
       if (cost < 2500){
         buyInfo(2);
@@ -69,12 +117,24 @@ function mousePressed(){ // changes balance rigght now based on how many times y
       }
       else if (cost < 20000){
         buyInfo(25);
-        bonus = 25;
+        bonus = 50;
 
       }
-    else{
-      console.log(mouseX, mouseY);
-    }
+      else if (cost < 75000 ){
+        buyInfo(50);
+        bonus = 75;
+      }
+      else if (cost < 1000000 ){
+        buyInfo(100);
+        bonus = 300;
+      }
+
+      else{
+        bonus = int(cost);
+        buyInfo(bonus);
+
+      }
+  
   }
 }
 }
@@ -83,8 +143,8 @@ function mousePressed(){ // changes balance rigght now based on how many times y
 function buyInfo(multiplier){
   money = money - cost;
   cost = cost + costIncrease;
-  income = income + multiplier*bonus;
-  costIncrease = int(costIncrease * (multiplier * 1.001111));
+  income = int(income *1.71);
+  costIncrease = int(costIncrease * (multiplier));
   bonus = multiplier;
   console.log("cost = " + cost); 
   console.log("income = " + income);
