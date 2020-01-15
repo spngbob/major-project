e53eaf36cb42c35653239e426f4200db6de4897c
Doucette2502259 <2502259@sps-student.sk.ca>
2020-01-15 14:11:28 -0600

    organization and commenting

--- MP/sketch.js
+++ MP/sketch.js
@@ -5,6 +5,42 @@
 let cookies = 0; //  balance of cookies 
 let cps = 0; //  cookies per second
 
+// individual cost, amount owned, and the colour of the background
+	// Baker 1 
+let baker1cost = 15;
+let baker1owned = 0;
+let baker1color = 255;
+
+	// Baker 2
+let baker2cost = 200;
+let baker2owned = 0;
+let baker2color = 255;
+
+	// Baker 3
+let baker3cost = 1000;
+let baker3owned = 0;
+let baker3color = 255;
+
+	// Baker 4
+let baker4cost = 2000;
+let baker4owned = 0;
+let baker4color = 255;
+
+	// Baker 5
+let baker5cost = 50000;
+let baker5owned = 0;
+let baker5color = 255;
+
+	// Baker 6
+let baker6cost = 100000;
+let baker6owned = 0;
+let baker6color = 255;
+
+
+
+// f u n c t i o n s
+
+
 function preload() {
 	// loads my sounds 
 	soundFormats('mp3');
@@ -25,47 +61,9 @@ function preload() {
 
 	// background
 	tabletop = loadImage('assets/background2.jpg'); // loads background
-
+	
 	cookieLocation();
 }
-
-
-
-
-// individual cost, amount owned, and the colour of the background
-
-// Baker 1 
-let baker1cost = 15;
-let baker1owned = 0;
-let baker1color = 255;
-
-// Baker 2
-let baker2cost = 200;
-let baker2owned = 0;
-let baker2color = 255;
-
-// Baker 3
-let baker3cost = 1000;
-let baker3owned = 0;
-let baker3color = 255;
-
-// Baker 4
-let baker4cost = 2000;
-let baker4owned = 0;
-let baker4color = 255;
-
-// Baker 5
-let baker5cost = 50000;
-let baker5owned = 0;
-let baker5color = 255;
-
-// Baker 6
-let baker6cost = 100000;
-let baker6owned = 0;
-let baker6color = 255;
-
-
-
 
 
 function setup() {
@@ -79,9 +77,6 @@ function setup() {
 	chill.setVolume(0.05);
 	chill.loop();
 }
-
-
-
 
 
 function windowResized() {
@@ -101,13 +96,9 @@ function windowResized() {
 		cookieY = 300;
 	}
 
-
 	drawCookie();
 	draw();
 }
-
-
-
 
 
 function draw() {
@@ -134,9 +125,124 @@ function draw() {
 	cookies = cookies + cps * 1 / 60;
 	cps = baker1owned * 1 + baker2owned * 5 + baker3owned * 10 + baker4owned * 25;
 
-
 	// I    T    E    M    S 
 
+	storeOptions();
+	colorOptions();
+	lines();
+	icons();
+}
+
+
+function keyPressed() {
+// adds cookies if you press space
+	if (key === " ") {
+		cookies = cookies + 1 + baker5owned * 50 + baker6owned * 100;
+		click.play();
+		console.log("lentils");
+	}
+}
+
+
+function mousePressed() { 
+// shrinks cookies when clicked ! 
+	if (mouseX > cookieX && mouseX < cookieX + cookieW && mouseY > cookieY && mouseY < cookieY + cookieH) {
+		cookieX = cookieX - 5;
+		cookieY = cookieY - 5;
+		cookieH = cookieH + 10;
+		cookieW = cookieW + 10;
+	}
+}
+
+
+function mouseReleased() { 
+// makes cookie normal sized again 
+	if (mouseX > cookieX && mouseX < cookieX + cookieW && mouseY > cookieY && mouseY < cookieY + cookieH) {
+		cookieX = cookieX + 5;
+		cookieY = cookieY + 5;
+		cookieH = cookieH - 10;
+		cookieW = cookieW - 10;
+		cookies = cookies + 1 + baker5owned * 50 + baker6owned * 100;
+		click.play();
+		console.log("beans");
+	}
+}
+
+
+function mouseClicked() {   
+// mouse clicked to buystuff
+	// Buy baker1
+	if (mouseX > 20 && mouseX < 20 + 250 && mouseY > 50 && mouseY < 50 + 75 && cookies >= baker1cost) {
+		cookies = cookies - baker1cost;
+		baker1owned++;
+		yay.play();
+	}
+
+	//Buy baker2
+	if (mouseX > 20 && mouseX < 20 + 250 && mouseY > 130 && mouseY < 130 + 75 && cookies >= baker2cost) {
+		cookies = cookies - baker2cost;
+		baker2owned++;
+		yay.play();
+	}
+
+	//Buy baker3
+	if (mouseX > 20 && mouseX < 20 + 250 && mouseY > 210 && mouseY < 210 + 75 && cookies >= baker3cost) {
+		cookies = cookies - baker3cost;
+		baker3owned++;
+		yay.play();
+	}
+
+	//Buy baker4
+	if (mouseX > 20 && mouseX < 20 + 250 && mouseY > 290 && mouseY < 290 + 75 && cookies >= baker4cost) {
+		cookies = cookies - baker4cost;
+		baker4owned++;
+		yay.play();
+	}
+
+	//Buy baker5
+	if (mouseX > 20 && mouseX < 20 + 250 && mouseY > 370 && mouseY < 370 + 75 && cookies >= baker5cost
+	) {
+		cookies = cookies - baker5cost;
+		baker5owned++;
+		yay.play();
+	}
+
+	//Buy baker6
+	if (mouseX > 20 && mouseX < 20 + 250 && mouseY > 450 && mouseY < 450 + 75 && cookies >= baker6cost) {
+		cookies = cookies - baker6cost;
+		baker6owned++;
+		yay.play();
+	}
+}
+
+
+function drawCookie() {
+	noStroke();
+	image(cookieImage, cookieX, cookieY, cookieW, cookieH);
+}
+
+
+function cookieLocation(){
+	// sets cookie location
+	if (windowWidth > 700) { 
+		cookieW = windowWidth / 5;
+		cookieH = windowWidth / 5;
+		cookieX = windowWidth / 2 - cookieW / 2;
+		cookieY = windowHeight / 2 - cookieH / 2;
+	}
+
+	// set a fixed cookie location that wont interfere with the store
+	else {
+		cookieW = 100;   
+		cookieH = 100;
+		cookieX = 300;
+		cookieY = 300;
+	}
+}
+
+
+function storeOptions(){    
+// creates store buttons
 	// baker 1 store option
 	fill(baker1color);
 	rect(20, 50, 180, 75);
@@ -216,9 +322,33 @@ function draw() {
 	text("Level " + baker6owned, 25, 500);
 
 	baker6cost = 100000 * pow(1.07, baker6owned);
+}
 
 
+function icons(){
+	//Icons
+	image(baker1, 205, 51, 69, 75);
+	image(baker2, 205, 131, 69, 75);
+	image(baker3, 205, 211, 69, 74);
+	image(baker4, 205, 291, 69, 75);
+	image(baker5, 205, 370, 70, 75);
+	image(baker6, 205, 450, 69, 75);
 
+
+}
+
+
+function lines(){
+	// lines for shop
+	stroke(1);
+	for (i = 0; i < 461; i = i + 80) {
+		line(200, i + 50, 200, i + 125);
+	}
+}
+
+
+function colorOptions(){  
+// color coordination for availability and mouse location
 	// baker1 highlighter    
 	if (cookies < baker1cost) {
 		baker1color = (75, 16, 1, 100); // unavailable, can't buy yet
@@ -286,134 +416,5 @@ function draw() {
 	}
 	if (mouseX > 20 && mouseX < 20 + 250 && mouseY > 450 && mouseY < 450 + 75 && cookies >= baker6cost) {
 		baker6color = (50, 16, 1, 50);
-	}
-
-
-	// lines for shop
-	stroke(1);
-	for (i = 0; i < 461; i = i + 80) {
-		line(200, i + 50, 200, i + 125);
-	}
-
-	//Icons
-	image(baker1, 205, 51, 69, 75);
-	image(baker2, 205, 131, 69, 75);
-	image(baker3, 205, 211, 69, 74);
-	image(baker4, 205, 291, 69, 75);
-	image(baker5, 205, 370, 70, 75);
-	image(baker6, 205, 450, 69, 75);
-}
-
-
-
-
-// adds cookies if you press space
-function keyPressed() {
-	if (key === " ") {
-		cookies = cookies + 1 + baker5owned * 50 + baker6owned * 100;
-		click.play();
-		console.log("lentils");
-	}
-}
-
-
-
-
-// C   o   o  k   i   e        s   i   z   e
-function mousePressed() { // shrinks cookies ! 
-	if (mouseX > cookieX && mouseX < cookieX + cookieW && mouseY > cookieY && mouseY < cookieY + cookieH) {
-		cookieX = cookieX - 5;
-		cookieY = cookieY - 5;
-		cookieH = cookieH + 10;
-		cookieW = cookieW + 10;
-	}
-}
-
-function mouseReleased() { // makes cookie normal sized again 
-	if (mouseX > cookieX && mouseX < cookieX + cookieW && mouseY > cookieY && mouseY < cookieY + cookieH) {
-		cookieX = cookieX + 5;
-		cookieY = cookieY + 5;
-		cookieH = cookieH - 10;
-		cookieW = cookieW - 10;
-		cookies = cookies + 1 + baker5owned * 50 + baker6owned * 100;
-		click.play();
-		console.log("beans");
-	}
-}
-
-
-
-
-
-
-
-// mouse clicked to buystuff 
-function mouseClicked() {
-	// Buy baker1
-	if (mouseX > 20 && mouseX < 20 + 250 && mouseY > 50 && mouseY < 50 + 75 && cookies >= baker1cost) {
-		cookies = cookies - baker1cost;
-		baker1owned++;
-		yay.play();
-	}
-
-	//Buy baker2
-	if (mouseX > 20 && mouseX < 20 + 250 && mouseY > 130 && mouseY < 130 + 75 && cookies >= baker2cost) {
-		cookies = cookies - baker2cost;
-		baker2owned++;
-		yay.play();
-	}
-
-	//Buy baker3
-	if (mouseX > 20 && mouseX < 20 + 250 && mouseY > 210 && mouseY < 210 + 75 && cookies >= baker3cost) {
-		cookies = cookies - baker3cost;
-		baker3owned++;
-		yay.play();
-	}
-
-	//Buy baker4
-	if (mouseX > 20 && mouseX < 20 + 250 && mouseY > 290 && mouseY < 290 + 75 && cookies >= baker4cost) {
-		cookies = cookies - baker4cost;
-		baker4owned++;
-		yay.play();
-	}
-
-	//Buy baker5
-	if (mouseX > 20 && mouseX < 20 + 250 && mouseY > 370 && mouseY < 370 + 75 && cookies >= baker5cost
-	) {
-		cookies = cookies - baker5cost;
-		baker5owned++;
-		yay.play();
-	}
-
-	//Buy baker6
-	if (mouseX > 20 && mouseX < 20 + 250 && mouseY > 450 && mouseY < 450 + 75 && cookies >= baker6cost) {
-		cookies = cookies - baker6cost;
-		baker6owned++;
-		yay.play();
-	}
-}
-
-function drawCookie() {
-	noStroke();
-	image(cookieImage, cookieX, cookieY, cookieW, cookieH);
-
-}
-
-
-function cookieLocation(){
-	// sets cookie location
-	if (windowWidth > 700) { 
-		cookieW = windowWidth / 5;
-		cookieH = windowWidth / 5;
-		cookieX = windowWidth / 2 - cookieW / 2;
-		cookieY = windowHeight / 2 - cookieH / 2;
-	}
-
-	// set a fixed cookie location that wont interfere with the store
-	else {
-		cookieW = 100;   
-		cookieH = 100;
-		cookieX = 300;
-		cookieY = 300;
 	}
 }
